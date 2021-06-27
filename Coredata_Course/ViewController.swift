f5aa7505bedec26e4d26b04d4701ff1c175f7b41
Ravikumar <test@youremail.com>
2021-06-27 13:16:31 +0530

    Initial Commit

---
+++ Coredata_Course/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coredata_Course
+//
+//  Created by Ravikumar on 6/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
